22bf6be460d0dcedfa8a0f3dc87998cf618a37cc
zoni <mohamed.hajji.med@gmail.com>
2024-06-05 16:13:49 +0100

    styling core navbar

--- src/components/layouts/navbar.jsx
+++ src/components/layouts/navbar.jsx
@@ -5,40 +5,39 @@ import profilePic from '../../assets/pdp.jpeg' // will import it later from data
 
 export default function Navbar() {
   return (
-    <>
+    <nav>
       <div className='left-nav'>
-        <h1>cryptor</h1>
-        <img src={cryptorLogo} alt="profile-picture" className='profile-pic'/>
-      </div>
 
+        <a href='#dashboard' className='left-left-nav'>
+          <img src={cryptorLogo} alt="logo" className='cryptor-logo'/>
+          <h1>Cryptor</h1>
+        </a>
 
-      <div className='mid-nav'>
-        <ul>
+        <ul className='left-right-nav'>
           <li><a href="#dashboard">Dashboard</a></li>
           <li><a href="#markets">Markets</a></li>
           <li><a href="#wallet">Wallet</a></li>
           <li><a href="#news">News</a></li>
         </ul>
+
       </div>
 
 
       <div className='right-nav'>
-        
-        <div className='select-container'>
-          <select className='select-lang'>
-            <option value="eng">ENG</option>
-            <option value="fr">FR</option>
-          </select>
 
-          <select className='select-curr'>
-            <option value="usd">USD</option>
-            <option value="eur">EUR</option>
-          </select>
-        </div>
+        <select className='select-lang'>
+          <option value="eng">ENG</option>
+          <option value="fr">FR</option>
+        </select>
 
-        <img src={profilePic} alt="profile-pic" className="profile-pic" />
+        <select className='select-curr'>
+          <option value="usd">USD</option>
+          <option value="eur">EUR</option>
+        </select>
+
+        <a href="#profile"><img src={profilePic} alt="profile-pic" className="profile-pic" /></a>
       
       </div>
-    </>
+    </nav>
   )
 }
