83988852065e025ec3dd0479248d54bf01c78c28
zoni <mohamed.hajji.med@gmail.com>
2024-06-29 16:29:32 +0200

    styling current-price-and-percentage element

--- src/components/in-dashboard/CryptoInfos.jsx
+++ src/components/in-dashboard/CryptoInfos.jsx
@@ -66,6 +66,39 @@ const CryptoInfos = ({ selectedCoin }) => {
     }
   };
 
+  const priceChangePercentage24Hours = () =>{
+    const absoluteChange = currentPrice - price24HoursAgo
+    const percentageChange = (absoluteChange / price24HoursAgo)*100
+    return percentageChange.toFixed(2)
+  }
+
+  const getArrowIcon = (percentageChange)=>{
+    if(percentageChange > 0)
+      return <span id='price-up-arrow'>&#9650;</span>
+    else if(percentageChange < 0)
+      return <span id='price-down-arrow'>&#9660;</span>
+    else 
+      return null
+  }
+
+  let cryptoPercentageBoxClass
+  if (priceChangePercentage24Hours() > 0) {
+    cryptoPercentageBoxClass = 'price-up-for-percentage-change-24h';
+} else if (priceChangePercentage24Hours() < 0) {
+    cryptoPercentageBoxClass = 'price-down-for-percentage-change-24h';
+} else {
+    cryptoPercentageBoxClass = 'crypto-price-percentage-change-24h'; // Default background color class
+}
+
+let priceDiff = currentPrice - price24HoursAgo
+if(priceDiff > 0)
+  priceDiff = '+ $'+priceDiff.toFixed(2)
+else if (priceDiff < 0)
+  priceDiff = '- $'+Math.abs(priceDiff.toFixed(2))
+else
+  priceDiff = '$'+0
+
+
   useEffect(() => {
     fetchCoins();
   }, [selectedCoin, currency, days]); 
@@ -135,28 +168,33 @@ const CryptoInfos = ({ selectedCoin }) => {
   }
 
   return (
+
+    
+
     <div className='crypto-infos'>
       <div className='crypto-desc'>
         <p>Current Price</p>
-          
-          <div>
-            <h1 className='selected-crypto-price'>{currentPrice ? '$ '+currentPrice.toFixed(2) : 'Loading Price...'}</h1>
-            <div className='crypto-price-percentage-change-24h'>
-              <p>{((currentPrice - price24HoursAgo)/price24HoursAgo)*100}%</p>
-            </div>
+        <div className='current-price-and-percentage'>
+          <h1 className='selected-crypto-price'>{currentPrice ? '$ '+currentPrice.toFixed(2) : 'Loading Price...'}</h1>
+          <div className={cryptoPercentageBoxClass}>
+            <span id='arrow'>{getArrowIcon(priceChangePercentage24Hours())}</span>
+            <p>{priceChangePercentage24Hours ? Math.abs(priceChangePercentage24Hours())+'%' : 'Loading %...'}</p>
           </div>
-
-          <div className='crypto-price-change-24h'>
-            <p className='price-diff'>+ $303</p>
-            <p className='hour-24'>24h</p>
-          </div>
-          
-          <div>
-            <p className='selected-crypto-name'>Bitcoin</p>
-            <div className='days'></div>
-          </div>
+        </div>
+        
+        <div className='crypto-price-change-24h'>
+          <p className='price-diff'>{priceDiff}</p>
+          <p className='hour-24'>24h</p>
+        </div>
+        
+        <div>
+          <p className='selected-crypto-name'>Bitcoin</p>
+          <div className='days'></div>
+        </div>
 
       </div>
+
+
       <div className='crypto-chart'>
         {isLoading ? (
           <p>Loading chart data...</p>
